ff71a329dc938ecc4cf86d4160a9392cd8eed7f7
uu <giladbarkan@live.com>
2016-06-26 01:24:50 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(ic){
+  return "I love " + ic;
+}
+
+function shouting(s){
+  return s.toUpperCase();
+}
+
+function roundDown(n){
+  return Math.floor(n);
+}
+
+function theTruth(){
+  return true;
+}
